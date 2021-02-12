12029c5b1d6644e37eb1ff1e72d52fefd7e7be33
jfriisKU <xdr476@alumni.ku.dk>
2021-02-12 16:15:31 +0100

    Create useful_queries.sql

---
+++ Implementation/Data/useful_queries.sql
@@ -0,0 +1,3 @@
+select count_, count(*) as counts from _775147 group by count_ ORDER BY counts DESC;
+select count(DISTINCT  count_) from _775147;
+select count(*) as number_of_dates, count(DISTINCT  count_) as distinct_count from _775147;
