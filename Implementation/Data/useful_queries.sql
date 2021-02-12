d2851d142e70c6dfe415c8daf59eb9d74cdc143b
jfriisKU <xdr476@alumni.ku.dk>
2021-02-12 21:45:03 +0100

    Add database creating unix

--- Implementation/Data/useful_queries.sql
+++ Implementation/Data/useful_queries.sql
@@ -1,3 +1,8 @@
 select count_, count(*) as counts from _775147 group by count_ ORDER BY counts DESC;
 select count(DISTINCT  count_) from _775147;
 select count(*) as number_of_dates, count(DISTINCT  count_) as distinct_count from _775147;
+
+[jfriis]~>$ sudo su - postgres
+postgres@jfriis:~$ createdb bachelorBesoeg2014
+
+
